2f232037b2fdaee2b9bcb839b084dbcfaba7f194
Alejandro Rusi <alejandro.rusi@mercadolibre.com>
2018-05-25 00:37:22 -0300

    Prim first implementation

--- src/algorithms/Prim.h
+++ src/algorithms/Prim.h
@@ -1,5 +1,8 @@
 #ifndef PATHS_IN_GRAPHS_PRIM_H
 #define PATHS_IN_GRAPHS_PRIM_H
 
+#include "../datastructures/graphs/AListGraph.h"
+
+AListGraph& Prim(Graph &graph);
 
 #endif //PATHS_IN_GRAPHS_PRIM_H
