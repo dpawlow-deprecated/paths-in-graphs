815d7cf02761ef9db8545dc7c18d6fa379fd4e21
Guido Jose Maceira <gmaceira@dc.uba.ar>
2018-05-25 20:19:39 -0300

    Sin ; AMatrixGraph

--- src/algorithms/Dantzig.cpp
+++ src/algorithms/Dantzig.cpp
@@ -1,24 +1,38 @@
 #include "Dantzig.h"
 
-void dantzigAlgorithm(AMatrixGraph matrix) {
 
-    /*
+AMatrixGraph DantzigAlgorithm(AMatrixGraph &matrixG) {
+    AMatrixGraph matrixGraph = matrixG;
+
     for (int k = 0; k < n - 1; ++k) {
+
         for (int i = 0; i < k; ++i) {
-            matrix[i][k + 1] = getMinOf(i, k, i, k + 1);
-            matrix[k + 1][i] = getMinOf(i, k, k + 1, i);
+
+            if (matrixGraph.EdgeExists(i, j)) {
+                matrixGraph.SetEdgeWeight(i, k + 1) = getMinOf(i, k, i, k + 1);
+            } else {
+                matrixGraph.AddEdge(Edge(i, k + 1, getMinOf(i, k, i, k + 1)));
+            }
+
+            if (matrixGraph.EdgeExists(i, j)) {
+                matrixGraph.SetEdgeWeight(k + 1, i) = getMinOf(i, k, k + 1, i);
+            } else {
+                matrixGraph.AddEdge(Edge(k + 1, i, getMinOf(i, k, k + 1, i)));
+            }
+        }
+
+        for (int i = 0; i < k; ++i) {
+            for (int j = 0; j < k; ++j) {
+                if (matrixGraph.EdgeExists(i, j)) {
+                    matrixGraph.SetEdgeWeight(i, j) = min(matrixGraph.GetEdgeWeight(i, j),
+                                                          matrixGraph.GetEdgeWeight(i, k + 1) +
+                                                          matrixGraph.GetEdgeWeight(k + 1, j));
+                } else {
+                    matrixGraph.AddEdge(
+                            Edge(i, j, matrixGraph.GetEdgeWeight(i, k + 1) + matrixGraph.GetEdgeWeight(k + 1, j)));
+                }
+            }
         }
     }
-
-    t = getMinOf();
-
-    if (t < 0) {
-        throw ("Existen circuitos negativos");
-    }
-
-    for (int i = 0; i < k; ++i) {
-        for (int j = 0; j < k; ++j) {
-            matrix[i][j] = getMin(matrix[i][j], matrix[i][k + 1] + matrix[k + 1][j]);
-        }
-    }*/
+    matrixGraph.PrintGraph();
 }
