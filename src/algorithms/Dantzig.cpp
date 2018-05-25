cd1466ffcdfc6f3bdaae9178364d1b7e1bfe7a38
Guido Jose Maceira <gmaceira@dc.uba.ar>
2018-05-25 12:24:44 -0300

    - Floyd class

--- src/algorithms/Dantzig.cpp
+++ src/algorithms/Dantzig.cpp
@@ -1,5 +1,8 @@
-void dantzig(AMatrixGraph matrix) {
+#include "Dantzig.h"
 
+void dantzigAlgorithm(AMatrixGraph matrix) {
+
+    /*
     for (int k = 0; k < n - 1; ++k) {
         for (int i = 0; i < k; ++i) {
             matrix[i][k + 1] = getMinOf(i, k, i, k + 1);
@@ -17,19 +20,5 @@ void dantzig(AMatrixGraph matrix) {
         for (int j = 0; j < k; ++j) {
             matrix[i][j] = getMin(matrix[i][j], matrix[i][k + 1] + matrix[k + 1][j]);
         }
-    }
-}
-
-int getMinOf(int i, int limit, int rowValue, int colValue) {
-    int result = 0;
-
-    for (int j = 0; j < limit; ++j) {
-        value =  matrix[rowValue][j] + matrix[j][colValue];
-
-        if(value > result) {
-            result = value;
-        }
-    }
-
-    return result;
+    }*/
 }
