430eb04b9caacac5aeecf89bd8f8ee6707e46511
Guido Jose Maceira <gmaceira@dc.uba.ar>
2018-05-25 12:20:44 -0300

    - Floyd class

--- src/algorithms/Floyd.h
+++ src/algorithms/Floyd.h
@@ -4,12 +4,7 @@
 #include "../types/types.h"
 #include "../datastructures/graphs/AMatrixGraph.h"
 
-class Floyd {
-private:
-    static Weight minimum(Weight first, Weight second);
-public:
-    static void floydAlgorithm(AMatrixGraph* matrixGraph);
-
-};
+static Weight minimum(Weight first, Weight second);
+static void floydAlgorithm(AMatrixGraph* matrixGraph);
 
 #endif //PATHS_IN_GRAPHS_FLOYD_H
