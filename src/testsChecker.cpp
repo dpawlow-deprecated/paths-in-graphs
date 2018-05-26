6f893466e46a7133c1935afe1849b558b547868b
Guido Jose Maceira <gmaceira@dc.uba.ar>
2018-05-26 14:22:20 -0300

    + MatrixChecker

--- src/testsChecker.cpp
+++ src/testsChecker.cpp
@@ -1,5 +1,34 @@
-//
-// Created by guido on 26/05/18.
-//
-
 #include "testsChecker.h"
+#include "datastructures/graphs/AListGraph.h"
+#include "algorithms/BellmanFord.h"
+
+bool checkMatrixAlgorithms(AMatrixGraph &matrixGraph){
+    AMatrixGraph floyd = FloydAlgorithm(matrixGraph);
+    AMatrixGraph dantzig = DantzigAlgorithm(matrixGraph);
+
+    dantzig.PrintGraph();
+    floyd.PrintGraph();
+
+    for (int i = 0; i < matrixGraph.GetNumberOfNodes(); i++) {
+        for (int j = 0; j < matrixGraph.GetNumberOfNodes(); j++) {
+            if(dantzig.EdgeExists(i,j) != floyd.EdgeExists(i,j)){
+                return false;
+            }
+
+            if(dantzig.EdgeExists(i,j)){
+                int test = dantzig.GetEdgeWeight(i,j);
+                int test2 = floyd.GetEdgeWeight(i,j);
+                if(dantzig.GetEdgeWeight(i,j) != floyd.GetEdgeWeight(i,j)){
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+bool checkListAlgorithms(AListGraph &listGraph){
+    //Continuar...
+   /* AMatrixGraph dijkstra = Dijkstra(listGraph);
+    AMatrixGraph bellmanford = BellmanFordAlgorithm(bellmanford);*/
+}
--- src/testsChecker.h
+++ src/testsChecker.h
@@ -1,14 +1,10 @@
-//
-// Created by guido on 26/05/18.
-//
-
 #ifndef PATHS_IN_GRAPHS_TESTSCHECKER_H
 #define PATHS_IN_GRAPHS_TESTSCHECKER_H
+#include "testsChecker.h"
+#include "datastructures/graphs/AMatrixGraph.h"
+#include "algorithms/Floyd.h"
+#include "algorithms/Dantzig.h"
 
-
-class testsChecker {
-
-};
-
+bool checkMatrixAlgorithms(AMatrixGraph &matrixGraph);
 
 #endif //PATHS_IN_GRAPHS_TESTSCHECKER_H
