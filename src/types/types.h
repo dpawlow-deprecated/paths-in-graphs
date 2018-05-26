05bdd4ef518f0f28e9aa29c90d4eadcf1b57df42
Guido Jose Maceira <gmaceira@dc.uba.ar>
2018-05-26 19:53:29 -0300

    Sacados tipos que no correspondian en types.h

--- src/types/types.h
+++ src/types/types.h
@@ -6,22 +6,4 @@ using namespace std;
 using Node = unsigned long;
 using Weight = long;
 
-struct MatrixEdge {
-    bool not_null;
-    Weight weight;
-
-    MatrixEdge(){
-        not_null = true;
-        weight = 0;
-    }
-
-    explicit MatrixEdge(Weight w) {
-        not_null = true;
-        weight = w;
-    }
-};
-
-using AdjacencyRow = vector<MatrixEdge>;
-using AdjacencyMatrix = vector<AdjacencyRow>;
-
 #endif //PATHS_IN_GRAPHS_TYPES_H
